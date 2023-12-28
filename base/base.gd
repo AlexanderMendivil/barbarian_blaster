6930399d739108edca5fce03e500840704cdc6af
Alexander Mendivil <alexanderima@outlook.es>
2023-12-28 15:15:23 -0700

    labels and colors

--- base/base.gd
+++ base/base.gd
@@ -1,10 +1,21 @@
 extends Node3D
 
 @export var max_health: int = 5
+
 @onready var label_3d: Label3D = $Label3D
 
+var currentHealth: int:
+	set(health_in):
+		currentHealth = health_in
+		label_3d.text = str(currentHealth) + "/" + str(max_health)
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(currentHealth) / float(max_health))
+		if currentHealth < 1:
+			get_tree().reload_current_scene()
+
 func _ready() -> void:
-	label_3d.text = str(max_health)
+	currentHealth = max_health
 
 func _process(_delta: float) -> void:
 	pass
@@ -12,5 +23,4 @@ func _process(_delta: float) -> void:
 
 func take_damage() -> void:
 	if max_health != 0:
-		max_health -= 1 
-		label_3d.text = str(max_health)
+		currentHealth -= 1 
