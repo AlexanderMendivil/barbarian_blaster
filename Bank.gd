b4f56b56d9ba975a53704fb3fb1468e2c9f84966
Alexander Mendivil <alexanderima@outlook.es>
2024-01-11 21:42:57 -0700

    Labels on screen

---
+++ Bank.gd
@@ -0,0 +1,16 @@
+extends MarginContainer
+
+@export var starting_gold := 150
+@onready var label: Label = $Label
+var gold : int:
+	set(gold_in):
+		gold = max(gold_in, 0)
+		label.text = "Gold: {a}".format({"a":gold})
+
+func _ready():
+	gold = starting_gold
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
